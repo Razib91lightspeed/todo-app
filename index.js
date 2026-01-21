4c7cb2d893fdb7bdb8f012d548aad9b8b85e9386
Razib Hasan <razibulhasan2003@gmail.com>
2026-01-21 22:41:36 +0200

    Exercise 1.5: respond to HTTP GET on /

--- index.js
+++ index.js
@@ -3,8 +3,21 @@ import http from "http";
 const PORT = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
-  res.end("Todo app backend\n");
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end(`
+      <html>
+        <head><title>Todo App</title></head>
+        <body>
+          <h1>Todo App</h1>
+          <p>Server is running inside Kubernetes 🎉</p>
+        </body>
+      </html>
+    `);
+  } else {
+    res.writeHead(404);
+    res.end("Not found");
+  }
 });
 
 server.listen(PORT, () => {
